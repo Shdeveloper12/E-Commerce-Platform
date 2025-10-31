3e569938dde1fe566b2902d0f9d06ae502397891
Shdevloper <shafayet237@gmail.com>
2025-10-31 23:59:25 +0600

    added start up website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-8 p-4">
       <div className="relative w-24 h-24 md:w-32 md:h-32">
+        <menubar.tsx />
        <h1>StarTech BD</h1>
       </div>
     </div>
